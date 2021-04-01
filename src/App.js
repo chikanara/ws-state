4ac1e8345b0d6d61b3dc91e91c6fa74b20516ea4
chikanara <wassimahmedmp21@gmail.com>
2021-04-01 19:45:32 +0100

    same7ouni

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Counter from './components/Counter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export class App extends Component {
+  state={
+    show:false
+  }
+  toggle = () => this.setState({show:!this.state.show})
+  render() {
+    return (
+      <div>
+        <button onClick={this.toggle}>{this.state.show ?   "hide" : "show"}</button> <br/>
+        {
+          this.state.show ? <Counter/> : "Click to show the counter"
+        }
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
